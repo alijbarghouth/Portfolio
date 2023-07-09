13addbd60ec8d398029b63fa53c300a36a9f59ff
alijbarghouth <alijbarghouth@gmail.com>
2023-07-09 13:20:43 +0300

    add navbar

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+   <section className="home" id="home">
+    <div className="home-content">
+      <h3>Hello, It's Me</h3>
+      <h1>Ali Barghouth</h1>
+      <h3>And I'm a <span>Full Stack Developer</span></h3>
+      <p>Asp.Net | ReactJs</p>
+    </div>
+   </section>
+  );
+}
+
+export default Home;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg sticky-top navbar-dark">
+      <div className="container">
+        Portofolio
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main"
+          aria-controls="main"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <i className="fa-solid fa-bars" />
+        </button>
+        <div className="collapse navbar-collapse" id="main">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className="nav-link active p-2 p-lg-3 "
+                aria-current="page"
+                to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active p-2 p-lg-3"
+                aria-current="page"
+                href="#">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active p-2 p-lg-3"
+                aria-current="page"
+                href="#">
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active p-2 p-lg-3"
+                aria-current="page"
+                href="#">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active p-2 p-lg-3"
+                aria-current="page"
+                href="#">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
